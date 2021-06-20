eade4e619d822a9d12c21d97546eb57cfe2c5df8
Amey-Chaudhari <64803502+Amey-Chaudhari@users.noreply.github.com>
2021-06-20 16:55:38 +0530

    Create Palindromic_pattern.cpp

---
+++ C++/Palindromic_pattern.cpp
@@ -0,0 +1,35 @@
+/*
+
+Program to print palindromic pattern
+
+        1 
+      2 1 2
+    3 2 1 2 3
+  4 3 2 1 2 3 4
+5 4 3 2 1 2 3 4 5
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+int i,j,n;
+cin >> n;
+for(i=1;i<=n;i++){
+    for(j=1;j<=n-i;j++){
+        cout<<"  ";
+    }
+    int k=i;
+    for(;j<=n;j++){
+        cout<<k--<<" ";
+    }
+    k=2;
+    for(;j<=n+i-1;j++){
+        cout<<k++<<" ";
+    }
+    cout<<endl;
+}
+
+
+}
