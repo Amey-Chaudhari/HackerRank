a5abc692f310531f979636797f6939efd35aceab
Amey-Chaudhari <64803502+Amey-Chaudhari@users.noreply.github.com>
2021-12-25 23:10:16 +0530

    Update string_formatting.py

--- python/string_formatting.py
+++ python/string_formatting.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     print_formatted(n)
     
     
-    
+ #This Solution is not passed the cases of hackerrank solution. but the output of this problem statement comes out true   
  '''   
     # String Formatting in Python - HackerRank Solution
 def print_formatted(number):
