9963e418880f1ec5f3219c30bdd72e4424555cc6
Amey-Chaudhari <64803502+Amey-Chaudhari@users.noreply.github.com>
2022-02-23 23:06:45 +0530

    Update polybag.py

--- CodeChef/polybag.py
+++ CodeChef/polybag.py
@@ -1,6 +1,12 @@
 """Chef bought N items from a shop. Although it is hard to carry all these items in hand, so Chef has to buy some polybags to store these items.
 
-1 polybag can contain at most 10 items. What is the minimum number of polybags needed by Chef?"""
+1 polybag can contain at most 10 items. What is the minimum number of polybags needed by Chef?
+
+Input Format
+The first line will contain an integer T - number of test cases. Then the test cases follow.
+The first and only line of each test case contains an integer N - the number of items bought by Chef.
+
+"""
 
 t = int(input())
 for i in range(0,t):
