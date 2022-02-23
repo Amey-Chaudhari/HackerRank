91918ef2eebe056f20e58e7741cb1d7f8593bcb8
Amey-Chaudhari <64803502+Amey-Chaudhari@users.noreply.github.com>
2022-02-23 23:06:07 +0530

    Update polybag.py

--- CodeChef/polybag.py
+++ CodeChef/polybag.py
@@ -1,3 +1,7 @@
+"""Chef bought N items from a shop. Although it is hard to carry all these items in hand, so Chef has to buy some polybags to store these items.
+
+1 polybag can contain at most 10 items. What is the minimum number of polybags needed by Chef?"""
+
 t = int(input())
 for i in range(0,t):
     n=int(input())
